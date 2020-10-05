4d26d0707c9f6114e2be59961c6af54feaf653d2
Darkkyon037 <johan.ramirez@utp.edu.co>
2020-10-05 13:51:50 -0500

    first commit

---
+++ src/app/mock-data.ts
@@ -0,0 +1,200 @@
+export const DATA: any  = {
+  'categories': [
+    {
+      'categori_id': 1,
+      'name': 'Belgan India Pale'
+    },
+    {
+      'categori_id': 2,
+      'name': ' Pale lager'
+    },
+    {
+      'categori_id': 3,
+      'name': 'IPA'
+    },
+    {
+      'categori_id': 4,
+      'name': 'Pale Ale'
+    }
+    {
+      'categori_id': 5,
+      'name': 'Blonde Ale'
+    },
+    {
+      'categori_id': 6,
+      'name': 'Brown Ale'
+    },
+    {
+      'categori_id': 7,
+      'name': 'Porter'
+    },
+    {
+      'categori_id': 8,
+      'name': 'Amber Ale'
+    }
+    {
+      'categori_id': 9,
+      'name': 'Stout'
+    },
+    {
+      'categori_id': 10,
+      'name': 'Hefeweizen'
+    }
+  ],
+  'products': [
+    {
+      'id': 1,
+      'name': 'Memorial 1831',
+      'price': '9,999',
+      'available': true,
+      'best_seller': false,
+      'categories': [
+        1,
+        4
+      ],
+      'img': 'http://lorempixel.com/200/100/food/',
+      'description': 'Estilo: Porter Grado de alcohol: 5,4% Presentación: 330 ml'
+    },
+    {
+      'id': 2,
+      'name': 'Kalima   Más Música Loca',
+      'price': '9,999',
+      'available': true,
+      'best_seller': false,
+      'categories': [
+        
+      ],
+      'img': 'http://lorempixel.com/200/100/food/',
+      'description': 'Estilo: Doble IIPA, India Pale Ale Grado de alcohol: 8% Presentación: 330 ml.'
+    },
+    {
+      'id': 3,
+      'name': 'La Santa Sek',
+      'price': '9,999',
+      'available': true,
+      'best_seller': false,
+      'categories': [
+        4
+      ],
+      'img': 'http://lorempixel.com/200/100/food/',
+      'description': 'Grado de alcohol: 5,0% Estilo: Kölsch Presentación: 350 ml.'
+    },
+    {
+      'id': 4,
+      'name': 'Hakuna Marinera',
+      'price': '9,999',
+      'available': true,
+      'best_seller': false,
+      'categories': [
+        1,
+        2
+      ],
+      'img': 'http://lorempixel.com/200/100/food/',
+      'description': 'Estilo: Irish Red Ale Grado de alcohol: 6,5% Presentación: 330 ml'
+    },
+    {
+      'id': 5,
+      'name': 'Monasterio Inferno',
+      'price': '9,999',
+      'available': true,
+      'best_seller': false,
+      'categories': [
+        1,
+        4
+      ],
+      'img': 'http://lorempixel.com/200/100/food/',
+      'description': 'Estilo: Porter Grado de alcohol: 7% Presentación: 330 ml'
+    },
+    {
+      'id': 6,
+      'name': 'Master Beer Mysteria IPA',
+      'price': '13,999',
+      'available': true,
+      'best_seller': false,
+      'categories': [
+        1,
+        4
+      ],
+      'img': 'http://lorempixel.com/200/100/food/',
+      'description': 'Estilo: India Pale Ale Grado de alcohol: 6,2% Presentación: 330 ml'
+    },
+    {
+      'id': 7,
+      'name': 'Nink Hannya',
+      'price': '8,999 ',
+      'available': true,
+      'best_seller': false,
+      'categories': [
+        1,
+        3
+      ],
+      'img': 'http://lorempixel.com/200/100/food/',
+      'description': 'Estilo: Belgian India Pale Ale, India Pale Ale Grado de alcohol: 7% Presentación: 330 ml'
+    },
+    {
+      'id': 8,
+      'name': 'Nink Inquisición',
+      'price': '8,999',
+      'available': true,
+      'best_seller': false,
+      'categories': [
+        1,
+        3
+      ],
+      'img': 'http://lorempixel.com/200/100/food/',
+      'description': 'Estilo: Red IPA Grado de alcohol: 6,5% Presentación: 330 ml'
+    },
+    {
+      'id': 9,
+      'name': 'Nink Nott',
+      'price': '8,999',
+      'available': true,
+      'best_seller': false,
+      'categories': [
+        2,
+        4
+      ],
+      'img': 'http://lorempixel.com/200/100/food/',
+      'description': 'Estilo: Dry Stout, Stout Grado de alcohol: 6% Presentación: 330 ml'
+    },
+    {
+      'id': 10,
+      'name': 'Wicca Agua Lunar',
+      'price': '12,999',
+      'available': true,
+      'best_seller': false,
+      'categories': [
+        2,
+        3
+      ],
+      'img': 'http://lorempixel.com/200/100/food/',
+      'description': 'Estilo: Cream Ale, Ale Grado de alcohol: 5,0% Presentación: 500 ml'
+    }
+    {
+      'id': 11,
+      'name': 'Wicca Locura Colectiva',
+      'price': '12,999',
+      'available': true,
+      'best_seller': false,
+      'categories': [
+        2,
+        3
+      ],
+      'img': 'http://lorempixel.com/200/100/food/',
+      'description': 'Estilo: Sour Ale  Grado de alcohol: 5,0% Presentación: 500 ml'
+    }
+    {
+      'id': 12,
+      'name': 'Olympus Zeus',
+      'price': '9,999',
+      'available': true,
+      'best_seller': false,
+      'categories': [
+        2,
+        3
+      ],
+      'img': 'http://lorempixel.com/200/100/food/',
+      'description': 'Estilo: American Golden Ale, Golden Ale Grado de alcohol: 4,8% Presentación: 330 ml'
+    }
+  ]
+};
